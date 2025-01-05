09ce6d7490db0fa830b76bfe8604f1777bbed1c5
Rigers777 <rigerszypce13@gmail.com>
2025-01-05 13:47:32 +0100

    Update abstract.py

--- abstract.py
+++ abstract.py
@@ -11,3 +11,10 @@ class Hotel(ABC):
     @abstractmethod
     def get_details(self):
         pass
+class Guesthouse(Hotel):
+    def __init__(self, name, city_id, guest_capacity):
+        super().__init__(name, city_id)
+        self.guest_capacity = guest_capacity
+
+    def get_details(self):
+        return f"Guesthouse: {self.name}, City ID: {self.city_id}, Capacity: {self.guest_capacity}"
